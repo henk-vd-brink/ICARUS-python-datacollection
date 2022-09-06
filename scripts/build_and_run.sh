402c24c21f78f013fff3067b17965082b4931825
Henk van den Brink <hcebrink@gmail.com>
2022-09-06 08:39:33 +0200

    Update build_and_run.sh

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -2,5 +2,5 @@
 
 export DC_IMAGE_TAG=$(date +%Y%m%d-%H%M)
 
-docker-compose -f docker-compose.build.yaml build
+docker-compose -f docker-compose.ci.build.yaml build
 docker-compose -f docker-compose.dev.yaml up
