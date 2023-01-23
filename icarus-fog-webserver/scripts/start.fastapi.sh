b4b7bc27f7cff39912f784d68115c927272f19da
Prominendt <hcebrink@gmail.com>
2023-01-23 19:44:04 +0100

    Set uwsgi permissions in /tmp

--- icarus-fog-webserver/scripts/start.fastapi.sh
+++ icarus-fog-webserver/scripts/start.fastapi.sh
@@ -2,8 +2,9 @@
 
 alembic upgrade head \
 && python3 -m gunicorn src.webserver.entrypoints.fastapi_app:app \
+    --user docker_user \
     --workers 4 \
     --worker-class uvicorn.workers.UvicornWorker \
-    --bind unix:/tmp/gunicorn.sock \
+    --bind unix:/tmp/uwsgi/gunicorn.sock \
     --log-level INFO \
     --access-logfile -
